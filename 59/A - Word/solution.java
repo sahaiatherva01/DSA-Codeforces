e7806d91b1daf26e90fcc733b72ba76ae0771a7c
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-08-15 09:45:55 +0530

    Add Word [A] from Codeforces

---
+++ 59/A - Word/solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        String s = sc.next();
+ 
+        int upper = 0;
+        int lower = 0;
+ 
+        for (char c : s.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                upper++;
+            } else {
+                lower++;
+            }
+        }
+ 
+        if (upper > lower) {
+            System.out.println(s.toUpperCase());
+        } else {
+            System.out.println(s.toLowerCase());
+        }
+    }
+}
