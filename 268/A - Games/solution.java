4b3eb5302b8b2c3c1f8c57fbe3edcd42068a257e
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-08-16 09:12:00 +0530

    Add Games [A] from Codeforces

---
+++ 268/A - Games/solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+ 
+        int[] home = new int[n];
+        int[] guest = new int[n];
+ 
+        for (int i = 0; i < n; i++) {
+            home[i] = sc.nextInt();
+            guest[i] = sc.nextInt();
+        }
+ 
+        int answer = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && home[i] == guest[j]) {
+                    answer++;
+                }
+            }
+        }
+ 
+        System.out.println(answer);
+    }
+}
