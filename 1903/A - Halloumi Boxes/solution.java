20914261dbab8d697ff50e95891ab2a148154a70
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-08-24 14:47:48 +0530

    Add Halloumi Boxes [A] from Codeforces

---
+++ 1903/A - Halloumi Boxes/solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+ 
+            int[] a = new int[n];
+ 
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+ 
+            if (k >= 2) {
+                System.out.println("YES");
+            } else {
+                boolean sorted = true;
+ 
+                for (int i = 1; i < n; i++) {
+                    if (a[i] < a[i - 1]) {
+                        sorted = false;
+                        break;
+                    }
+                }
+ 
+                System.out.println(sorted ? "YES" : "NO");
+            }
+        }
+    }
+}
