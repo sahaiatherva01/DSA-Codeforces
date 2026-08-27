f17f3c93d3c572866ef59e90024dfe2052d85981
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-08-27 22:33:36 +0530

    Add Medium Number [A] from Codeforces

---
+++ 1760/A - Medium Number/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+ 
+            int min = Math.min(a, Math.min(b, c));
+            int max = Math.max(a, Math.max(b, c));
+ 
+            int medium = a + b + c - min - max;
+ 
+            System.out.println(medium);
+        }
+    }
+}
