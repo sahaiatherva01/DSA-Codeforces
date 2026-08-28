9a13b97739492887cd17c727408a3aa3a4cc9b84
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-08-28 07:39:19 +0530

    Add Raising Bacteria [A] from Codeforces

---
+++ 579/A - Raising Bacteria/solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        int x = sc.nextInt();
+ 
+        int count = 0;
+ 
+        while (x > 0) {
+            count += x & 1;
+            x >>= 1;
+        }
+ 
+        System.out.println(count);
+    }
+}
